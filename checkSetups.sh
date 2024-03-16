51b996cb2d0aa64772cf7941bbfc2a734864ff6a
ahmet fatih yegen <fatihyegen.09@gmail.com>
2024-03-16 13:56:48 -0500

    add: check setups grep options

--- checkSetups.sh
+++ checkSetups.sh
@@ -1,7 +1,7 @@
-git --version
-wget --version
-vim --version # If preferred during installation.
-zsh --version 
-cat /etc/ssh/sshd_config | grep Port # Should be 4242 only
-sudo service ssh status # Should have to work.
+git --version | grep git --color
+wget --version | grep wget --color
+vim --version | grep vim --color # If preferred during installation.
+zsh --version | grep zsh --color
+cat /etc/ssh/sshd_config | grep Port --color # Should be seen 4242 only
+sudo service ssh status | grep enable -A 2 -B 2 --color  # Should have to work.
 hostname | grep $(users) --color # If done correctly then username will be red if not result is empty
