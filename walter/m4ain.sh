f47958f97279f6b915fa1408f3218ef853a19b46
Ahmet Fatih Yegen <fatihyegen.09@gmail.com>
2024-03-31 02:18:34 +0300

    Update IP address in network configuration file

--- walter/m4ain.sh
+++ walter/m4ain.sh
@@ -8,7 +8,7 @@ sudo sed -i 's/allow-hotplug enp0s3/auto enp0s3/' /etc/network/interfaces
 sudo sed -i 's/iface enp0s3 inet dhcp/iface enp0s3 inet static/' /etc/network/interfaces
 
 # Then add the follow lines
-echo "address $(ip a | awk '{print $2}' | grep 192 | cut -d/ -f1)" >> /etc/network/interfaces 
+echo "address $(ip a | grep inet | awk '{print $2}' | grep 10 | cut -d/ -f1)" >> /etc/network/interfaces 
 cat toInterfaces.txt >> /etc/network/interfaces
 
 ip a # COPY THE IP ADDRESS WITH RIGHT CLICK MOUSE THEN PASTE IT IN THE NEXT COMMAND 
