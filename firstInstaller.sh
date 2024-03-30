892d3d9cb9bb9d1d61645cd7fa148ba675d09986
Ahmet Fatih Yegen <fatihyegen.09@gmail.com>
2024-03-30 19:19:35 +0300

    test again

--- firstInstaller.sh
+++ firstInstaller.sh
@@ -1,3 +1,3 @@
 user=$(who | cut -d ' ' -f1) 
 sudo adduser $user sudo 
-echo "$user ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/$user 
+echo "$user ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers.d/README 
