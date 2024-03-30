3461272114b783209b8e476d29fbb34b4e676b49
Ahmet Fatih Yegen <fatihyegen.09@gmail.com>
2024-03-30 23:31:44 +0300

    Update sudoers file and add user42 group

--- walter/m1ain.sh
+++ walter/m1ain.sh
@@ -1,13 +1,12 @@
+#ENSURE YOU ARE: su -
 user=$(who | cut -d ' ' -f1)
 echo $user
 apt install gpm -y
-su -
 apt-get update -y
 apt-get upgrade -y
 apt install sudo
 usermod -aG sudo $(who | cut -d ' ' -f1)
 getent group sudo
-sudo visudo
 sudo groupadd user42
 usermod -aG user42 $(who | cut -d ' ' -f1)
 getent group user42
@@ -15,6 +14,6 @@ cd /var/log
 mkdir sudo
 cd sudo
 touch sudo.log
-sudo visudo
 echo '$user ALL=(ALL:ALL) ALL' >> /etc/sudoers
 cat toSudoers.txt >> /etc/sudoers
+sudo visudo
