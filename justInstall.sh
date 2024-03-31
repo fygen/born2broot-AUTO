857c43aea40641dc1f42573f288937ae473e0eef
Ahmet Fatih Yegen <fatihyegen.09@gmail.com>
2024-03-31 06:39:10 +0300

    trials to make everything runs syncronously

--- justInstall.sh
+++ justInstall.sh
@@ -1,11 +1,30 @@
-echo "justInstall no echo do your job"
-./1system_setup_and_sudo_config.sh
+#!/bin/bash
+
+function echo_execute() {
+    echo "Executing commands from file: $1"
+    eval "$1"
+}
+
+$user=$(who | cut -d ' ' -f1)
+
+# Source the functions file
+# source funcs.sh
+function enter_file(){
+    # Read each line from the file using a for loop
+    IFS=$'\n'  # Set IFS to newline
+    for line in $(<"$1"); do
+        echo_execute "$line"
+    done
+}
+
+
+enter_file "./1system_setup_and_sudo_config.sh"
 echo "1system_setup_and_sudo_config.sh done"
-./2install_required_packages.sh
+enter_file "./2install_required_packages.sh"
 echo "2install_required_packages.sh done"
-./3configure_ssh_and_firewall.sh
+enter_file "./3configure_ssh_and_firewall.sh"
 echo "3configure_ssh_and_firewall.sh done"
-./4password_policy_configuration.sh
+enter_file "./4password_policy_configuration.sh"
 echo "4password_policy_configuration.sh done"
-./5setup_monitoring_and_reboot.sh
+enter_file "./5setup_monitoring_and_reboot.sh"
 echo "5setup_monitoring_and_reboot.sh done"
