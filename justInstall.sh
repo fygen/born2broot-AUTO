ccb7b6033f8351674eaca0f9b2c751954babfc75
Ahmet Fatih Yegen <fatihyegen.09@gmail.com>
2024-03-31 06:44:23 +0300

    is our savior wait command?

--- justInstall.sh
+++ justInstall.sh
@@ -18,13 +18,23 @@ function enter_file(){
 }
 
 
-enter_file "./1system_setup_and_sudo_config.sh"
+enter_file "./1system_setup_and_sudo_config.sh" &
+pid1=$!
+wait $pid1
 echo "1system_setup_and_sudo_config.sh done"
-enter_file "./2install_required_packages.sh"
-echo "2install_required_packages.sh done"
-enter_file "./3configure_ssh_and_firewall.sh"
+enter_file "./2install_required_packages.sh" &
+pid2=$!
+wait $pid2
+echo "2install_required_packages.sh done" 
+enter_file "./3configure_ssh_and_firewall.sh" &
+pid3=$!
+wait $pid3
 echo "3configure_ssh_and_firewall.sh done"
-enter_file "./4password_policy_configuration.sh"
+enter_file "./4password_policy_configuration.sh" &
+pid4=$!
+wait $pid4
 echo "4password_policy_configuration.sh done"
-enter_file "./5setup_monitoring_and_reboot.sh"
+enter_file "./5setup_monitoring_and_reboot.sh" &
+pid5=$!
+wait $pid5
 echo "5setup_monitoring_and_reboot.sh done"
