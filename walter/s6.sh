d2a8f94792fe8c319697750e4e3b927159a71796
Ahmet Fatih Yegen <fatihyegen.09@gmail.com>
2024-03-31 03:06:53 +0300

    Add user variable to s6.sh script

--- walter/s6.sh
+++ walter/s6.sh
@@ -1,11 +1,12 @@
+user=$(who | cut -d ' ' -f1)
 cp monitoring.sh /usr/local/bin/monitoring.sh
 chmod 777 /usr/local/bin/monitoring.sh
 # Access the VM by Ubuntu terminal
 # Change the monitoring.sh file
 nano /usr/local/bin/monitoring.sh
-sudo visudo
 echo "$user ALL=(ALL) NOPASSWD: /usr/local/bin/monitoring.sh" >> /etc/sudoers
-sudo /usr/local/bin/monitoring.sh
+sudo visudo
 # At the file end: */10 * * * * /usr/local/bin/monitoring.sh
+sudo /usr/local/bin/monitoring.sh
 sudo crontab -u root -e
 sudo reboot
