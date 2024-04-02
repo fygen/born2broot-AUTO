579588404b9047b191a739d7d4a6ec491cb04f17
Ahmet Fatih Yegen <ayegen@k1m10s01.42istanbul.com.tr>
2024-04-02 21:27:21 +0300

    autoinstall things

--- justInstall.sh
+++ justInstall.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 wd=$(pwd)
+autoInstall=$1
+if [ "$autoInstall" == "y" ]; then
+    echo "Auto install"
+else
+    echo "Manual install"
+fi
+
 function echo_execute() {
-    local answer="y"  # Automatically accept 'yes'
+    if [ "$autoInstall" == "y" ]; then
+        answer = "y"
+    else
+        read answer # Automatically accept 'yes'
+    fi
     if [ "$answer" != "${answer#[Yy]}" ]; then
         eval "$1"
     else
@@ -18,22 +29,22 @@ function enter_file(){
     #     echo_execute "$line"
     # done < "$1"
     while IFS= read -r line; do
-        echo_execute "$line" 
+        echo_execute "$line"
     done < "$1"
 }
 
 
-enter_file "./1system_setup_and_sudo_config.sh" 
+enter_file "./1system_setup_and_sudo_config.sh"
 echo "1system_setup_and_sudo_config.sh done"
 cd $wd
-enter_file "./2install_required_packages.sh" 
+enter_file "./2install_required_packages.sh"
 echo "2install_required_packages.sh done"
 cd $wd 
-enter_file "./3configure_ssh_and_firewall.sh" 
+enter_file "./3configure_ssh_and_firewall.sh"
 echo "3configure_ssh_and_firewall.sh done"
 cd $wd
-enter_file "./4password_policy_configuration.sh" 
+enter_file "./4password_policy_configuration.sh"
 echo "4password_policy_configuration.sh done"
 cd $wd
-enter_file "./5setup_monitoring_and_reboot.sh" 
+enter_file "./5setup_monitoring_and_reboot.sh"
 echo "5setup_monitoring_and_reboot.sh done"
