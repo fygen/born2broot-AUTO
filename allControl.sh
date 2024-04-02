2e8477401efbca386c24c4dc73fc8599d0d3ebee
Ahmet Fatih Yegen <ayegen@k1m10s01.42istanbul.com.tr>
2024-04-02 22:41:27 +0300

    fixes2

--- allControl.sh
+++ allControl.sh
@@ -1,10 +1,11 @@
+user=$(who | cut -d ' ' -f1 | awk 'NR==1' )
 sudo visudo
 nano /var/log/sudo/sudo.log
 nano /etc/ssh/sshd_config
 sudo ufw status numbered
 sudo nano /etc/pam.d/common-password
 sudo nano /etc/login.defs
-chage -l root
-chage -l $(who | cut -d ' ' -f1)
+sudo chage -l root
+chage -l $user
 nano /usr/local/bin/monitoring.sh
 sudo crontab -u root -e
