89ac19cefc343829dc3d1e36fe3424a749bf0077
Ahmet Fatih Yegen <ayegen@k1m10s01.42istanbul.com.tr>
2024-04-02 22:51:26 +0300

    fixes4

--- 2install_required_packages.sh
+++ 2install_required_packages.sh
@@ -1,12 +1,12 @@
 sudo apt install gpm -y
-echo "$(gpm --version)"
+gpm -version
 sudo apt install openssh-server -y 
-echo "$(ssh --version)"
+ssh --version
 sudo apt-get install ufw -y
-echo "$(ufw --version)"
+ufw --version
 sudo apt-get install libpam-pwquality -y
-echo "$(libpam-pwquality --version)"
+libpam-pwquality --version
 sudo apt-get install git -y
-echo "$(git --version)"
+git --version
 sudo apt-get install net-tools -y
-echo "$(netstat --version)"
+netstat --version
